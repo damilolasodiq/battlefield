64a9722b29b289e109cf2a6f0925a779f29039ca
Sodiq Fagbola <fagbola@systemspecs.com.ng>
2017-11-20 00:05:12 +0100

    fix buys

--- src/test/java/com/randomapps/battlefield/BattleFieldTest.java
+++ src/test/java/com/randomapps/battlefield/BattleFieldTest.java
@@ -155,9 +155,9 @@ public class BattleFieldTest {
     }
 
     @Test
-    public void solderHitGeneralsArmorVest() throws GameInitializationException, WeaponNotAssignableException {
-        Player player1 = new Player("Player 1");
-        Player player2 = new Player("Player 2");
+    public void soldierHitGeneralsArmorVest() throws GameInitializationException, WeaponNotAssignableException {
+        Player player1 = new Player(player1Name);
+        Player player2 = new Player(player2Name);
 
         Level level = new Level(2);
         BattleField battleField = new BattleField(player1, player2, level);
